04b270d866a32e5c3ca563ba727c6b4812452d3b
pavloteyfel <pavloteyfel@gmail.com>
2021-12-04 20:42:31 +0100

    models and endpoints defined

--- app.py
+++ app.py
@@ -1,34 +1,121 @@
-from flask_restful import Api, Resource
-from flask import Flask
-from models import db
+from flask_restful import Api, Resource, marshal_with, fields, marshal, reqparse
+from flask import Flask, redirect, request
+from models import db, Event, User
 
 app = Flask(__name__)
 app.config.from_object('config')
+db.init_app(app)
 api = Api(app)
 
+
+
+parser = reqparse.RequestParser()
 #-----------------------------------------------------------------------------#
 # APIs
 #-----------------------------------------------------------------------------#
 
+# TODO: move to the model
+event = {
+    'id': fields.Integer,
+    'name': fields.String,
+}
 
-class Base(Resource):
+# TODO: move to the model
+user = {
+    'id': fields.Integer,
+    'name': fields.String,
+}
+
+# TODO: move to the model
+user_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'email': fields.String,
+    'location': fields.String,
+    'profile_picture': fields.String,
+    'events_organizer': fields.Nested(event),
+    'events_attendees': fields.Nested(event),
+    'events_presenters': fields.Nested(event),
+}
+
+# TODO: move to the model
+event_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'picture': fields.String,
+    'details': fields.String,
+    'location': fields.String,
+    'event_time': fields.String,
+    'organizer': fields.Nested(user),
+    'attendees': fields.Nested(user),
+    'presenters': fields.Nested(user),
+}
+
+
+class BaseResource(Resource):
     def get(self):
         return {}
 
 
-class Event(Resource):
+class EventListResource(Resource):
     def get(self):
-        return {}
+        events = Event.query.all()
+        return marshal(events, event_fields, envelope='events')
 
+    def post(self):
+        event_parser = parser.copy()
+        event_parser.add_argument('name', type=str, required=True)
+        event_parser.add_argument('details', type=str, required=True)
+        event_parser.add_argument('location', type=str, required=True)
+        event_parser.add_argument('picture', type=str)
+        event_parser.add_argument('event_time', type=str, required=True)
+        event_parser.add_argument('orgamizer', type=str, required=True)
+        args = event_parser.parse_args()
 
-class User(Resource):
+class UserListResource(Resource):
     def get(self):
-        return {}
+        users = User.query.all()
+        return marshal(users, user_fields, envelope='users')
+    
+    def post(self):
+        """
+        Example error message:
+        400
+        {
+            "message": {
+                "email": "Missing required parameter in the JSON body or the post body or the query string"
+            }
+        }
+        """
+        user_parser = parser.copy()
+        user_parser.add_argument('name', type=str, required=True)
+        user_parser.add_argument('email', type=str, required=True)
+        args = user_parser.parse_args()
+
+        # TODO: move to model
+        user = User(**args)
+        db.session.add(user)
+        db.session.commit()
+        user_id = user.id
+        db.session.close()
+        return {'id': user_id}, 201
+
+class UserResource(Resource):
+    def get(self, id):
+        user = User.query.get_or_404(id)
+        return marshal(user, user_fields)
 
 
-api.add_resource(Base, '/api/v1')
-api.add_resource(Event, '/api/v1/events')
-api.add_resource(User, '/api/v1/users')
+class EventResource(Resource):
+    def get(self, id):
+        event = Event.query.get_or_404(id)
+        return marshal(event, event_fields)
+
+api.add_resource(BaseResource, '/api/v1')
+api.add_resource(EventResource, '/api/v1/events/<int:id>')
+api.add_resource(EventListResource, '/api/v1/events')
+api.add_resource(UserListResource, '/api/v1/users')
+api.add_resource(UserResource, '/api/v1/users/<int:id>')
 
 
 #-----------------------------------------------------------------------------#
@@ -38,4 +125,21 @@ api.add_resource(User, '/api/v1/users')
 
 @app.route('/')
 def index():
-    return 'Front End'
+    e = Event.query.first()
+    print(e.attendees.filter_by(name='Anna').all())
+    return '<a href="/refresh">Refresh DB</b>'
+
+@app.route('/refresh')
+def create_db():
+    db.drop_all()
+    db.create_all()
+    u1 = User(name='Pavlo')
+    u2 = User(name='Anna')
+    u3 = User(name='Levente')
+    e = Event(name='First Run', organizer=u1)
+    e.presenters.append(u3)
+    e.attendees.append(u2)
+    db.session.add(e)
+    db.session.commit()
+    db.session.close()
+    return redirect('/')
--- models.py
+++ models.py
@@ -1,15 +1,50 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+
 
 
 db = SQLAlchemy()
 
 
+attendee = db.Table('attendees', 
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('events.id'), primary_key=True),
+)
+
+presenter = db.Table('presenters', 
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
+    db.Column('event_id', db.Integer, db.ForeignKey('events.id'), primary_key=True),
+)
+
+# TODO: check all fields
 class Event(db.Model):
     __tablename__ = 'events'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    event_time = db.Column(db.DateTime)
+    details = db.Column(db.String)
+    location = db.Column(db.String)
+    picture = db.Column(db.String)
+    # TODO: online / offline
+    # The user's id who created the event
+    organizer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    organizer = db.relationship('User', back_populates='events_organizer')
+    # If not defined then it will be the organizer
+    # presenter_id = db.Column(db.Integer, db.ForeignKey('User.id'), nullable=False)
+    attendees = db.relationship('User', back_populates='events_attendees', secondary=attendee, lazy='dynamic')
+    presenters = db.relationship('User', back_populates='events_presenters', secondary=presenter, lazy='dynamic')
 
 
-class Users(db.Model):
+# TODO: check all fields
+class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    location = db.Column(db.String)
+    profile_picture = db.Column(db.String)
+    member_since = db.Column(db.DateTime)
+    interests = db.Column(db.ARRAY(db.String))
+    events_organizer = db.relationship('Event', back_populates='organizer', lazy='dynamic')
+    events_attendees = db.relationship('Event', back_populates='attendees', secondary=attendee, lazy='dynamic')
+    events_presenters = db.relationship('Event', back_populates='presenters', secondary=presenter, lazy='dynamic')
