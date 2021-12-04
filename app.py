4ec2d3c8848d5028d8a12b7e48cd7a525d24c111
pavloteyfel <pavloteyfel@gmail.com>
2021-12-04 17:18:58 +0100

    api and model skeleton

--- app.py
+++ app.py
@@ -1,10 +1,41 @@
+from flask_restful import Api, Resource
 from flask import Flask
 from models import db
 
 app = Flask(__name__)
 app.config.from_object('config')
+api = Api(app)
+
+#-----------------------------------------------------------------------------#
+# APIs
+#-----------------------------------------------------------------------------#
+
+
+class Base(Resource):
+    def get(self):
+        return {}
+
+
+class Event(Resource):
+    def get(self):
+        return {}
+
+
+class User(Resource):
+    def get(self):
+        return {}
+
+
+api.add_resource(Base, '/api/v1')
+api.add_resource(Event, '/api/v1/events')
+api.add_resource(User, '/api/v1/users')
+
+
+#-----------------------------------------------------------------------------#
+# Optional for testing
+#-----------------------------------------------------------------------------#
+
 
 @app.route('/')
 def index():
-    return 'Hello World!'
-
+    return 'Front End'
--- models.py
+++ models.py
@@ -1,4 +1,15 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 
 db = SQLAlchemy()
+
+
+class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+
+
+class Users(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
