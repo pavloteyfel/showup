abd932c89295eeda1b5756e81c8a163c220f2853
pavloteyfel <pavloteyfel@gmail.com>
2021-12-10 09:23:11 +0100

    attendees count added

--- models.py
+++ models.py
@@ -61,41 +61,46 @@ presenter = db.Table('presenters',
     db.Column('event_id', db.Integer, db.ForeignKey('events.id'), primary_key=True),
 )
 
-# TODO: check all fields
+
 class Event(BaseModel):
     __tablename__ = 'events'
 
     fields = ['name', 'event_time', 'details', 'country', 'city', 'picture', 
                 'topics', 'format', 'organizer_id', 'presenters']
-    name = db.Column(db.String, nullable=False)
+
+    name = db.Column(db.String(150), nullable=False)
     event_time = db.Column(db.DateTime)
-    details = db.Column(db.String)
-    country = db.Column(db.String, nullable=False)
-    city = db.Column(db.String, nullable=False)
-    picture = db.Column(db.String)
+    details = db.Column(db.String(1000))
+    country = db.Column(db.String(50), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    picture = db.Column(db.String(100))
     topics = db.Column(ARRAY(db.String))
-    format = db.Column(db.String) # inperson, online, hybrid
+    format = db.Column(db.String(10))
     
     organizer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     organizer = db.relationship('User', back_populates='organizes_events')
     attendees = db.relationship('User', back_populates='attends_events', secondary=attendee)
     presenters = db.relationship('User', back_populates='presents_events', secondary=presenter)
 
+    @property
+    def attendees_count(self):
+        return len(self.attendees)
 
-# TODO: check all fields
+
 class User(BaseModel):
     __tablename__ = 'users'
 
     fields = ['name', 'email', 'country', 'city', 'picture', 'interests', 
                 'is_presenter', 'presenter_info', 'presenter_topics']
-    auth_user_id = db.Column(db.String)
-    name = db.Column(db.String)
-    email = db.Column(db.String)
-    country = db.Column(db.String)
-    city = db.Column(db.String)
-    picture = db.Column(db.String)
+
+    auth_user_id = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    country = db.Column(db.String(50))
+    city = db.Column(db.String(50))
+    picture = db.Column(db.String(100))
     is_presenter = db.Column(db.Boolean, default=False)
-    presenter_info = db.Column(db.String)
+    presenter_info = db.Column(db.String(1000))
     presenter_topics = db.Column(ARRAY(db.String))
     
     organizes_events = db.relationship('Event', back_populates='organizer')
