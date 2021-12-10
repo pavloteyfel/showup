e5710a4c0d8c60c4c2ef6972225a4da4a329cb70
pavloteyfel <pavloteyfel@gmail.com>
2021-12-10 19:13:41 +0100

    check empty values

--- models.py
+++ models.py
@@ -47,7 +47,7 @@ class BaseModel(db.Model):
     
     def from_dict(self, data):
         for field in self.fields:
-            if field in data:
+            if field in data and data[field] is not None:
                 setattr(self, field, data[field])
 
 
